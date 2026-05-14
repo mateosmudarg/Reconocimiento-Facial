e1b0d8325e5e8e22d0aaa571764455223d5e9fbc
mateosmudarg <marteotricolor@gmail.com>
2026-05-14 18:58:26 -0300

    Refactor code formatting in ventana_principal.py for improved readability

--- gui/ventana_principal.py
+++ gui/ventana_principal.py
@@ -1,12 +1,13 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel
+from PyQt5.QtWidgets import (
+    QWidget, QVBoxLayout, QPushButton, QLabel
+)
 from PyQt5.QtCore import Qt
 
 from gui import movimientos, lista_usuarios, handlers
 
 
 def configurar_estilos(app):
-    app.setStyleSheet(
-        """
+    app.setStyleSheet("""
         QPushButton {
             font-family: Segoe UI;
             font-size: 11pt;
@@ -17,13 +18,14 @@ def configurar_estilos(app):
             font-size: 16pt;
             font-weight: bold;
         }
-    """
-    )
+    """)
+
 
 def crear_boton(text, callback):
     btn = QPushButton(text)
     btn.clicked.connect(callback)
     return btn
+
 
 class VentanaPrincipal(QWidget):
     def __init__(self):
@@ -44,30 +46,29 @@ class VentanaPrincipal(QWidget):
 
         layout.addWidget(title)
 
-        layout.addWidget(crear_boton("Nuevo usuario", handlers.on_nuevo_usuario))
+        layout.addWidget(crear_boton(
+            "Nuevo usuario",
+            handlers.on_nuevo_usuario
+        ))
 
-        layout.addWidget(
-            crear_boton(
-                "Lista de usuarios", lambda: lista_usuarios.lista_usuarios(self)
-            )
-        )
+        layout.addWidget(crear_boton(
+            "Lista de usuarios",
+            lambda: lista_usuarios.lista_usuarios(self)
+        ))
 
-        layout.addWidget(
-            crear_boton(
-                "Validar entrada", lambda: handlers.on_agregar_movimiento("Entrada")
-            )
-        )
+        layout.addWidget(crear_boton(
+            "Validar entrada",
+            lambda: handlers.on_agregar_movimiento("Entrada")
+        ))
 
-        layout.addWidget(
-            crear_boton(
-                "Validar salida", lambda: handlers.on_agregar_movimiento("Salida")
-            )
-        )
+        layout.addWidget(crear_boton(
+            "Validar salida",
+            lambda: handlers.on_agregar_movimiento("Salida")
+        ))
 
-        layout.addWidget(
-            crear_boton(
-                "Lista de Movimientos", lambda: movimientos.mostrar_movimientos(self)
-            )
-        )
+        layout.addWidget(crear_boton(
+            "Lista de Movimientos",
+            lambda: movimientos.mostrar_movimientos(self)
+        ))
 
         self.setLayout(layout)
